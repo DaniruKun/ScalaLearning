599e79d36de87674edade17289f84daa042ea5fa
DaniruKun <dan181818@gmail.com>
2018-06-26 15:36:26 +0300

    Added comments

--- hello_world.scala
+++ hello_world.scala
@@ -2,7 +2,7 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     val a = "hello "
     val b = "world"
-    val output = a.+(b)
+    val output = a.+(b) //combined strings
     println(output)
   }
 }
